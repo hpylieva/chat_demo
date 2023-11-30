268e657e475bfb99788cc3327a71e5e75ee8b5cf
Hanna Pylieva <pylyeva@gmail.com>
2023-11-30 11:38:14 +0200

    Add document preview.

--- pages/2_📄_chat_with_your_documents.py
+++ pages/2_📄_chat_with_your_documents.py
@@ -1,6 +1,8 @@
+import base64
 import os
 import utils
 import streamlit as st
+from streamlit_javascript import st_javascript
 from streaming import StreamHandler
 
 from langchain.chat_models import ChatOpenAI
@@ -10,7 +12,21 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 from langchain.vectorstores import DocArrayInMemorySearch
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 from utils import check_password_routine
+
+def displayPDF(upl_file, ui_width):
+    # Read file as bytes:
+    bytes_data = upl_file.getvalue()
+
+    # Convert to utf-8
+    base64_pdf = base64.b64encode(bytes_data).decode("utf-8")
+
+    # Embed PDF in HTML
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width={str(ui_width)} height={str(ui_width*4/3)} type="application/pdf"></iframe>'
+
+    # Display file
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 check_password_routine()
 
@@ -75,24 +91,28 @@ class CustomDataChatbot:
 
     @utils.enable_chat_history
     def main(self):
+        with st.sidebar:
+            uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
+            if not uploaded_files:
+                st.error("Please upload PDF documents to continue!")
+                st.stop()
 
-        # User Inputs
-        uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
-        if not uploaded_files:
-            st.error("Please upload PDF documents to continue!")
-            st.stop()
 
-        user_query = st.chat_input(placeholder="Ask me anything!")
+        if uploaded_files:
 
-        if uploaded_files and user_query:
-            qa_chain = self.setup_qa_chain(uploaded_files)
+                user_query = st.chat_input(placeholder="Ask me anything!")
+                ui_width = st_javascript("window.innerWidth")
+                for file in uploaded_files:
+                    displayPDF(file, ui_width - 10)
+                if uploaded_files and user_query:
+                    qa_chain = self.setup_qa_chain(uploaded_files)
 
-            utils.display_msg(user_query, 'user')
+                    utils.display_msg(user_query, 'user')
 
-            with st.chat_message("assistant"):
-                st_cb = StreamHandler(st.empty())
-                response = qa_chain.run(user_query, callbacks=[st_cb])
-                st.session_state.messages.append({"role": "assistant", "content": response})
+                    with st.chat_message("assistant"):
+                        st_cb = StreamHandler(st.empty())
+                        response = qa_chain.run(user_query, callbacks=[st_cb])
+                        st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
     obj = CustomDataChatbot()
