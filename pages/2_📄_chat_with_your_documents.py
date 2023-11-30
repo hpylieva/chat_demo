b2682de164ceb979bcf63d031d9afd7b978c0870
Hanna Pylieva <pylyeva@gmail.com>
2023-11-30 11:18:17 +0200

    Update doc understanding tab and requirements.

--- pages/2_📄_chat_with_your_documents.py
+++ pages/2_📄_chat_with_your_documents.py
@@ -28,7 +28,7 @@ class CustomDataChatbot:
         folder = 'tmp'
         if not os.path.exists(folder):
             os.makedirs(folder)
-        
+
         file_path = f'./{folder}/{file.name}'
         with open(file_path, 'wb') as f:
             f.write(file.getvalue())
@@ -42,7 +42,7 @@ class CustomDataChatbot:
             file_path = self.save_file(file)
             loader = PyPDFLoader(file_path)
             docs.extend(loader.load())
-        
+
         # Split documents
         text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=1500,
@@ -60,26 +60,39 @@ class CustomDataChatbot:
             search_kwargs={'k':2, 'fetch_k':4}
         )
 
+        # Setup memory for contextual conversation
+        memory = ConversationBufferMemory(
+            memory_key='chat_history',
+            return_messages=True
+        )
+
         # Setup LLM and QA chain
         llm = ChatOpenAI(model_name=self.openai_model, temperature=0, streaming=True)
         qa_chain = ConversationalRetrievalChain.from_llm(
-            llm, retriever=retriever, verbose=True)
+            llm, retriever=retriever, memory=memory, verbose=True)
         return qa_chain
 
 
+    @utils.enable_chat_history
     def main(self):
+
         # User Inputs
         uploaded_files = st.sidebar.file_uploader(label='Upload PDF files', type=['pdf'], accept_multiple_files=True)
         if not uploaded_files:
             st.error("Please upload PDF documents to continue!")
             st.stop()
 
-        user_query = st.text_input(label="Ask me anything!", value="What is in this document(s)?")
+        user_query = st.chat_input(placeholder="Ask me anything!")
 
         if uploaded_files and user_query:
             qa_chain = self.setup_qa_chain(uploaded_files)
-            st_cb = StreamHandler(st.empty())
-            qa_chain.run(user_query, callbacks=[st_cb])
+
+            utils.display_msg(user_query, 'user')
+
+            with st.chat_message("assistant"):
+                st_cb = StreamHandler(st.empty())
+                response = qa_chain.run(user_query, callbacks=[st_cb])
+                st.session_state.messages.append({"role": "assistant", "content": response})
 
 if __name__ == "__main__":
     obj = CustomDataChatbot()
