e9484e59fe88794d609d47c8244e50223fc10b34
Hanna Pylieva <pylyeva@gmail.com>
2023-12-11 18:57:36 +0100

    Add VC advisor.

---
+++ pages/5_VS_advisor.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+
+
+import streamlit as st
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+st.title("VC advisor")
+
+
+check_password_routine()
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("How can I help you today?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        stream =  client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True
+        )
+        for response in stream:
+            print(response)
+            if response.choices[0].delta.content is not None:
+                full_response += response.choices[0].delta.content
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
